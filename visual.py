fcc8ffcfda442d7472ea5b2f8a3c9743e59b369a
Imran Alam <75203259+Imranalam28@users.noreply.github.com>
2023-07-08 06:37:25 +0530

    Create visual.py

---
+++ visual.py
@@ -0,0 +1,82 @@
+import arcade
+import time
+
+WIDTH = 800
+HEIGHT = 600
+
+white = arcade.color.WHITE
+blue = arcade.color.BLUE
+green = arcade.color.GREEN
+red = arcade.color.RED
+black = arcade.color.BLACK
+
+
+class Timer:
+    def __init__(self, duration):
+        self.duration = duration
+        self.start_time = 0
+        self.is_running = False
+
+    def start(self):
+        self.start_time = time.time()
+        self.is_running = True
+
+    def stop(self):
+        self.is_running = False
+
+    def get_elapsed_time(self):
+        if self.is_running:
+            return time.time() - self.start_time
+        return 0
+
+    def is_expired(self):
+        return self.get_elapsed_time() >= self.duration
+
+class GameWindow(arcade.Window):
+    def __init__(self):
+        super().__init__(WIDTH, HEIGHT, "Simple Game")
+        self.player_x = WIDTH // 2
+        self.player_y = HEIGHT // 2
+        self.timer = Timer(5)
+
+    def setup(self):
+        arcade.set_background_color(white)
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_rectangle_filled(self.player_x, 
+        self.player_y, 50, 50, blue)
+        arcade.draw_rectangle_filled(WIDTH // 2, 
+        HEIGHT // 4, WIDTH, 10, green)
+        if self.timer.is_running:
+            elapsed_time = self.timer.get_elapsed_time()
+            r_time = self.timer.duration - elapsed_time
+            remaining_time = max(r_time, 0)
+            arcade.draw_text(
+                f"Countdown: {remaining_time:.1f} seconds",
+                10,
+                10,
+                black,
+                18,
+            )
+        if self.timer.is_expired():
+            self.draw_rectangle()
+    def draw_rectangle(self):
+        arcade.draw_rectangle_filled(WIDTH // 2, HEIGHT // 2, 100, 100, red)   
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.LEFT:
+            self.player_x -= 10
+        elif key == arcade.key.RIGHT:
+            self.player_x += 10
+        elif key == arcade.key.SPACE:
+            self.timer.start()
+
+def main():
+    window = GameWindow()
+    window.setup()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
+
